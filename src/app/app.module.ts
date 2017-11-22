72989ff7e7118c409ebf444f7d02496b1e51df60
David Bronfen <davidbronfen@gmail.com>
2017-11-22 10:36:37 +0200

    app.module has been changed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './components/main/main.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    HeaderComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule
